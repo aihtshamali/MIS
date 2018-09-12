0cb0939517c177c83760675dbffbe3c1e04fc545
Anas Majeed <anasmajeed.ch@gmail.com>
2018-09-12 13:18:24 +0500

    Search for SSR&D

--- app/UserDetail.php
+++ app/UserDetail.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserDetail extends Model
 {
     public function sector(){
-      return $this->hasMany('App\Sector');
+      return $this->belongsTo('App\Sector');
     }
     public function User()
     {
