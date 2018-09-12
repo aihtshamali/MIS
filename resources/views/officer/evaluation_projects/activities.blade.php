2e7f98b3992e45155d750135941dc4545eaa2691
Anas Majeed <anasmajeed.ch@gmail.com>
2018-09-12 13:54:46 +0500

    DD Removed

--- resources/views/officer/evaluation_projects/activities.blade.php
+++ resources/views/officer/evaluation_projects/activities.blade.php
@@ -294,7 +294,6 @@
                                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100"
                                     aria-valuemin="0" aria-valuemax="100" style="width:100%">
                                     @if($activity->progress == 25.0)
-                                      {{ dd('Here') }}
                                       <label style="padding:5px;min-width:60px">
                                         {{ $activity->progress }} %
                                       </label>
