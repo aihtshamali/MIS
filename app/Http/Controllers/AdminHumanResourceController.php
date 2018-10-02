6e040524ae9be93a2865edd2ac236ad5507d272f
Hassan <hassan@appslab.io>
2018-10-02 12:51:59 +0500

    pdwpAgendas

--- app/Http/Controllers/AdminHumanResourceController.php
+++ app/Http/Controllers/AdminHumanResourceController.php
@@ -54,6 +54,8 @@ class AdminHumanResourceController extends Controller
       $meeting_filename = "PDWP-MOM-".$request->hr_agenda_id;
       $request->file('attach_moms')->storeAs('public/uploads/projects/meetings_mom',$meeting_filename.'.'.$request->file('attach_moms')->getClientOriginalExtension());
       $HRamiG->attachment = $meeting_filename.'.'.$request->file('attach_moms')->getClientOriginalExtension();
+      dd($HRamiG);
+
       $HRamiG->save();
     }
     return redirect()->back();
@@ -67,18 +69,9 @@ class AdminHumanResourceController extends Controller
      */
     public function store(Request $request)
     {
-        // dd($request->file('attachments'));
-        // dd($request->hasFile('attachments.'. 1));
-        // dd($request->file('attachments.'. 0));
-        // else
-        // dd('no file');
-        // $request->file('attachments.'. 0)->store('public/uploads/projects/project_agendas');
-        // dd( ($request->file('attachments.'. 0))->hashName());
-
-        // dd($request->all());
         $hr_meeting = new HrMeetingPDWP();
         $hr_meeting->hr_meeting_type_id = $request->type_of_meeting;
-        // $mytime = Carbon\Carbon::now();
+        $mytime = Carbon\Carbon::now();
         $hr_meeting->start_time =  date('Y-m-d',strtotime($request->my_date));
         if($request->meeting_no){
             $temp_meeting_no = $request->meeting_no.'-'.$hr_meeting->start_time;
@@ -97,10 +90,11 @@ class AdminHumanResourceController extends Controller
         $hr_meeting->save();
         $i = 0;
         $j=0;
+        $c=0;
         $agenda_items = explode(',',$request->agenda_type_items[0]);
         foreach($agenda_items as $agenda_type){
             $hr_agenda = new HrAgenda();
-            $hr_agenda->agenda_item = $request->agenda_item[$i];
+            $hr_agenda->agenda_item = $request->agenda_item[$c];
             $hr_agenda->hr_meeting_p_d_w_p_id = $hr_meeting->id;
             $hr_agenda->agenda_type_id = $agenda_type;
             if($agenda_type == 1 || $agenda_type == 2){
@@ -126,13 +120,14 @@ class AdminHumanResourceController extends Controller
                     $hr_attachment->save();
                 }
                 $i += 1;
+                $c += 1;
             }else{
-                $filename = 'WP-'.$temp_meeting_no;
                 $hr_agenda->hr_project_type_id = $request->section2_agenda_status[$j];
                 $hr_agenda->scheme_name = $request->topic[$j];
                 $hr_agenda->hr_sector_id = $request->section2_sector[$j];
                 $hr_agenda->start_timeofagenda = $request->section2_my_time[$j];
                 $hr_agenda->save();
+                $filename = 'WP-'.$temp_meeting_no.$hr_agenda->agenda_item;
                 if($request->hasFile('section2_attachments.'. $j)){
                     // dd($request->file('section2_attachments.'.$j));
                     $hr_attachment = new HrAttachment();
@@ -142,6 +137,7 @@ class AdminHumanResourceController extends Controller
                     $hr_attachment->save();
                 }
                 $j += 1;
+                $c += 1;
             }
 
                 // if($request->$attachments){
@@ -174,8 +170,63 @@ class AdminHumanResourceController extends Controller
         $agenda_types = AgendaType::all();
         $agendas = $meeting->HrAgenda;
         // dd($agendas);
+        \JavaScript::put([
+            'projects' => $adp
+        ]);
+
         return view('admin_hr.meeting.show',compact('agendas','meeting','agenda_statuses','adp','sectors','meeting_types','agenda_types'));
 
+    }
+
+    public function save_agendax(Request $request)
+    {
+        // dd($request->all());
+        $mytime = Carbon\Carbon::now();
+        $hr_agenda = new HrAgenda();
+        $hr_agenda->agenda_item = $request->agenda_item;
+        $hr_agenda->hr_meeting_p_d_w_p_id = $request->meeting_id;
+        $hr_agenda->agenda_type_id = $request->agenda_type;
+        if($request->agenda_type == 1 || $request->agenda_type == 2){
+            $hr_agenda->hr_project_type_id = $request->agenda_status;
+            $hr_agenda->scheme_name = $request->name_of_scheme;
+            if(isset($request->adp_no))
+              $hr_agenda->adp_no = explode(',',$request->adp_no)[0];
+            $hr_agenda->financial_year = $request->financial_year;
+            $hr_agenda->estimated_cost = $request->estimated_cost;
+            $hr_agenda->adp_allocation = $request->adp_allocation;
+            $hr_agenda->hr_sector_id = $request->sector;
+            $hr_agenda->start_timeofagenda = $request->my_time;
+            if(isset($request->adp_no))
+              $filename = 'WP-'.$request->meeting_id.'-'.$hr_agenda->adp_no . date('Y-m-d-i-s',$mytime->toDateTimeString());
+            else
+            $filename = 'WP-'.$request->meeting_id . date('Y-m-d-i-s',$mytime->toDateTimeString());
+            // dd($hr_agenda);
+            $hr_agenda->save();
+            if($request->hasFile('attachments')){
+                $hr_attachment = new HrAttachment();
+                $hr_attachment->hr_agenda_id = $hr_agenda->id;
+                $request->file('attachments')->storeAs('public/uploads/projects/project_agendas',$filename.'.'.$request->file('attachments')->getClientOriginalExtension());
+                $hr_attachment->attachments = $filename.'.'.$request->file('attachments')->getClientOriginalExtension();
+                $hr_attachment->save();
+            }
+        }else{
+            // dd($mytime->format('Y-m-d H:i:s'));
+            $filename = 'WP-'.$request->meeting_id.$hr_agenda->agenda_item;
+            $hr_agenda->hr_project_type_id = $request->section2_agenda_status;
+            $hr_agenda->scheme_name = $request->topic;
+            $hr_agenda->hr_sector_id = $request->section2_sector;
+            $hr_agenda->start_timeofagenda = $request->section2_my_time;
+            $hr_agenda->save();
+            if($request->hasFile('section2_attachments')){
+                // dd($request->file('section2_attachments.'.$j));
+                $hr_attachment = new HrAttachment();
+                $hr_attachment->hr_agenda_id = $hr_agenda->id;
+                $request->file('section2_attachments')->storeAs('public/uploads/projects/project_agendas',$filename.'.'.$request->file('section2_attachments')->getClientOriginalExtension());
+                $hr_attachment->attachments = $filename.'.'.$request->file('section2_attachments')->getClientOriginalExtension();
+                $hr_attachment->save();
+            }
+        }
+        return redirect()->back();
     }
 
     /**
