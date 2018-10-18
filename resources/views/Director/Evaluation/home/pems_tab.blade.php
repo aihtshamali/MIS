15f99ca1cba275659ef456729ceed76cdb0fa216
AymunSaif <14024020213@umt.edu.pk>
2018-10-18 11:40:43 +0500

    aymun

--- resources/views/Director/Evaluation/home/pems_tab.blade.php
+++ resources/views/Director/Evaluation/home/pems_tab.blade.php
@@ -23,13 +23,13 @@
         <div class="col-md-12">
           {{--  Chart 1  --}}
           <div class="box box-warning">
-              <div class="box-header with-border">
+              <div class="box-header with-border ">
               <h3 class="box-title">Total Projects Assigned To Officers</h3>
 
               <div class="box-tools pull-right">
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
               </button>
-              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              {{-- <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> --}}
               </div>
               </div>
               @php
@@ -90,13 +90,13 @@
             <!-- /.box-body -->
           </div>  
           <div class="box box-warning">
-              <div class="box-header with-border">
+              <div class="box-header with-border collapsed-box">
               <h3 class="box-title">Officers Completed Projects </h3>
 
               <div class="box-tools pull-right">
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
               </button>
-              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              {{-- <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> --}}
               </div>
               </div>
               @php
