905fd02b472ee4d3bbf39d9c807fb0f4bc4fc872
Hassan <hassan@appslab.io>
2018-10-26 12:48:25 +0500

    forAtiq

--- resources/views/Site_Visit/Plan_A_Trip/new_trip.blade.php
+++ resources/views/Site_Visit/Plan_A_Trip/new_trip.blade.php
@@ -10,7 +10,7 @@
     /* .form-control {margin: 1% !important;} */
     .form-control {border:none !important;margin-bottom:2%;color:#353333 !important;color: #353333 !important;border: 1px solid #d9d5d536 !important}
     .form-radio {display: -webkit-inline-box !important;}
-    .inlinebox {display: inline-box !important;display: -webkit-inline-box !important;}
+    .inlinebox {display: inline-box !important;display: -webkit-inline-box !important; padding-left: 0 !important; padding-right: 0 !important}
     .noborder{border:none !important;}
     .displaynone{display: none;}
     .select2-container--default .select2-selection--multiple .select2-selection__choice{
@@ -29,6 +29,7 @@
     border-color: #01a9ac !important;
     }
     .nodisplay{display: none;}
+    .bg{background-color: #e0e0e0}
 </style>
 @endsection
 @section('content')
@@ -40,10 +41,10 @@
             <div class="local">
                     <div class="col-md-12">
                             <div class="form-radio">
-                                    <div class="radio radio-outline radio-inline">
+                                    <div class="radio radio-outline radio-inline" >
                                         <label>
                                             <input type="radio" class="sinpurpose" name="radio" checked="checked">
-                                            <i class="helper"></i>single purpose
+                                            <i class="helper"></i>Single purpose
                                         </label>
                                     </div>
                                     <div class="radio radio-outline radio-inline">
@@ -54,10 +55,10 @@
                                     </div>
                             </div>
                         </div>
-                    <div id="clonethisproposal">
+                    <div id="clonethisproposal_1">
                         <div class="col-md-12 inlinebox">
                             <div class="col-md-4">
-                                <select id="reason" name="select" class="form-control form-control-default">
+                                <select id="reason_1" name="select" class="form-control form-control-default reason">
                                     <option selected="selected" hidden>Select Reason For Visit</option>
                                     <option value="Monitoring">Monitoring</option>
                                     <option value="Evaluation">Evaluation</option>
@@ -66,10 +67,26 @@
                                 </select>
                             </div>
 
-                            <div id='gsrow' class="col-md-8 row">
+                            <div class='form-control nodisplay col-md-8 row' id='brief'>
+                                <input style="width:100%" placeholder="Enter a brief description for visit" type="text" />
+                            </div>
+                            
+                            <div id='gsrow' class="col-md-8 row nodisplay">
                                 <div class="col-md-3">
-                                    <select name="select" class="form-control form-control-default">
+                                    {{-- <select name="select" class="form-control form-control-default">
                                         <option value="opt1" selected>Year</option>
+                                    </select> --}}
+                                    <select class="form-control  select2" name="financial_year" id="financial_year" style="width:100% !important;">
+                                            <option value="0" hidden>Select Financial Year </option>
+                                          @for($i = 2 ; $i <= 30 ; $i++)
+                                            @if($i == 9)
+                                              <option value="200{{$i}}-{{$i+1}}">200{{$i}}-{{$i+1}}</option>
+                                            @elseif($i > 9)
+                                              <option value="20{{$i}}-{{$i+1}}">20{{$i}}-{{$i+1}}</option>
+                                            @else
+                                              <option value="200{{$i}}-0{{$i+1}}">200{{$i}}-0{{$i+1}}</option>
+                                            @endif
+                                          @endfor
                                     </select>
                                 </div>
                                 <h2 class="col-md-1">/</h2>
@@ -86,29 +103,54 @@
                                 </div>
                             </div>
 
-                            <div class='form-control nodisplay' id='brief' >
-                                <input type="text" />
-                            </div>
                             
                         </div>
+                        <label for="">Location</label>
                         <div class="col-md-12 inlinebox">
-                            <div class="col-md-6">
+                            <div class="col-md-12" style="padding-left:0">
                                 <select name="select" class="form-control form-control-default">
-                                    <option value="opt1" selected hidden>From</option>
+                                    <option value="opt1" selected hidden>Lahore</option>
                                 </select>
                             </div>
-                            <div class="col-md-6">
+                            {{-- <div class="col-md-6" style="padding-left:0">
                                 <select name="select" class="form-control form-control-default">
                                     <option value="opt1" selected hidden>To</option>
                                 </select>
-                            </div>
+                            </div> --}}
                         </div>
                         <div class="col-md-12 inlinebox">
-                            <div class="col-md-6">
+                            <div class="col-md-6" style="padding-left:0">
+                                <label for="">Date</label>
                                 <input class="form-control" type="datetime-local">
                             </div>
-                            <div class="col-md-6">
-                                <input class="form-control" type="time">
+                            <div class="col-md-6" style="padding-left:0">
+                                <label for="">Time</label>
+                                {{-- <input class="form-control" type="time"> --}}
+                                <select  name="my_time[]" class="form-control" style="text-align: center !important" id="">
+                                    <option value="" hidden>Expected Time of Departure</option>
+                                    @for ($i = 1; $i < 12; $i++)
+                                    @for($j = 0; $j <= 45; $j+=30)
+                                        @if($j == 0)
+                                        <option value="{{$i . ' : ' . $j.'0' .' AM'}}"> {{$i . " : " . $j .'0'}} AM</option>
+                                        @else
+                                        <option value="{{$i . ' : ' . $j . ' AM'}}"> {{$i . " : " . $j }} AM </option>
+                                        @endif
+                                    @endfor
+                                    @endfor
+                                    <option value="12 : 00 PM">12 : 00 PM</option>
+                                    {{-- <option value="12 : 15 PM">12 : 15 PM</option> --}}
+                                    <option value="12 : 30 PM">12 : 30 PM</option>
+                                    {{-- <option value="12 : 45 PM">12 : 45 PM</option> --}}
+                                    @for ($i = 1; $i <= 11; $i++)
+                                    @for($j = 0; $j <= 45; $j+=30)
+                                        @if($j == 0)
+                                        <option value="{{$i . ' : ' . $j.'0' . ' PM' }}"> {{$i . " : " . $j .'0'}} PM</option>
+                                        @else
+                                        <option value="{{$i . ' : ' . $j .' PM' }}"> {{$i . " : " . $j }} PM </option>
+                                        @endif
+                                    @endfor
+                                    @endfor
+                                </select>
                             </div>
                         </div>  
                         <div class="form-group">
@@ -130,7 +172,7 @@
                         </div>
 
             </div>
-            <div class="outstation">
+            <div class="outstation nodisplay">
                 <div class="col-md-12">
                     <div class="form-radio">
                             <div class="radio radio-outline radio-inline">
@@ -162,15 +204,16 @@
 <script src="{{asset('_monitoring/css/js/jquery.quicksearch.js')}}"></script> --}}
 <script src="{{asset('_monitoring/css/pages/advance-elements/select2-custom.js')}}"></script>
 <script>
-
-     $(document).on('change','#reason',function(){
+    var purpose_id = 1
+     $(document).on('change','.reason',function(){
+         console.log($(this).parent().parent().find('#gsrow'))
     if ($(this).val() == "Other" || $(this).val() == "Meeting"){
-      $('#gsrow').hide()
-      $('#brief').show('slow')
+      $(this).parent().parent().find('#gsrow').hide()
+      $(this).parent().parent().find('#brief').attr('style','display:flex !important').show('slow')
     }
     else if ($(this).val() == "Monitoring" || $(this).val() == "Evaluation"){
-      $('#brief').hide()
-      $('#gsrow').show('slow')
+      $(this).parent().parent().find('#brief').hide()
+      $(this).parent().parent().find('#gsrow').attr('style','display:flex !important').show('slow')
     }
   })
 
@@ -179,8 +222,14 @@
           $(".js-multiple").select2();
     });  
     $(document).on('click','.addnewproposal',function(){
-    $('#clonethisproposal').clone().appendTo('.addclonehere').show('slow');
-  });
+        var clone = $('#clonethisproposal_1').clone().attr('id','clonethisproposal_'+ ++purpose_id)
+        if(purpose_id % 2 == 0)
+            clone.addClass('bg')
+        // clone.children().find('#gsrow_'+(purpose_id-1)).attr('id','gsrow_'+purpose_id)
+        // clone.children().find('#brief_'+(purpose_id-1)).attr('id','brief_'+purpose_id)
+        clone.children().find('#reason_'+(purpose_id-1)).attr('id','reason_'+purpose_id)
+        clone.appendTo('.addclonehere').show('slow');
+    });
   $(document).ready(function(){
     $(".sinpurpose").click(function(){
         $(".addnewproposal").hide();
