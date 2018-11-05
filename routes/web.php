d4abc53fb2101aea5a452a26bff6f55564565bb2
Hassan <hassan@appslab.io>
2018-11-05 10:53:46 +0500

    forAymun

--- routes/web.php
+++ routes/web.php
@@ -164,7 +164,7 @@ Route::prefix('officer')->middleware('role:officer')->group(function () {
   Route::post('/review_form','OfficerController@review_forms')->name('review_forms');
   Route::post('/saveActivityAttachment','OfficerController@saveActivityAttachment')->name('saveActivityAttachment');
   Route::get('/new_trip','SiteVisitController@create')->name('new_trip');
-  Route::get('/view_trip','SiteVisitController@view')->name('view_trip');
+  Route::get('/view_trips','SiteVisitController@view')->name('view_trips');
   Route::get('/new_tripbackup','SiteVisitController@create')->name('new_tripbackup');
 
 
