c7f3ba13429a6347889c0ba57a47c4fc89840e5c
Anas Majeed <anasmajeed.ch@gmail.com>
2018-11-13 15:25:49 +0500

    Project

--- resources/views/_Monitoring/_Dataentry/create.blade.php
+++ resources/views/_Monitoring/_Dataentry/create.blade.php
@@ -469,6 +469,57 @@
 <script src="{{asset('_monitoring/css/js/classie.js')}}"></script>
 <script src="{{asset('_monitoring/css/js/modal.js')}}"></script>
 <script>
+$(document).on('change','#gs_no,#financial_year',function(){
+   var arr = $(this).val();
+   console.log(arr);
+   if($(this).attr('id') == 'gs_no' && $('#financial_year').val() == projects[arr].financial_year){
+     $('#titleproject').val(projects[arr].name_of_scheme);
+     $('#summary_title').empty();
+     $('#label_summary_title').show('slow');
+     $('#summary_title').append("<label class=\"control-label\">"+$('#titleproject').val()+"</label>");
+
+     $('#original_cost').val(parseFloat(projects[arr].total_cost).toFixed(3));
+     $('#summary_original_cost').empty();
+     $('#label_summary_original_cost').show();
+     $('#summary_original_cost').append("<label class=\"control-label\">"+$('#original_cost').val()+"</label>");
+
+     $('#summary_districts').empty();
+     $("#districts").val($("#districts option").filter(function () { return $(this).html() == projects[arr].district; }).val());
+     $("#districts").select2();
+     values = $('#districts').find(':selected').text();
+     $("#label_summary_districts").show('slow');
+     $("#summary_districts").append("<label class=\"control-label\">"+values+"</label>");
+   }
+   else{
+     $('#titleproject').val('');
+     $('#original_cost').val('');
+     $("#districts").val('').trigger('change');
+     $('#summary_districts').empty();
+     $('#summary_original_cost').empty();
+     $('#summary_title').empty();
+
+     var arr = $('#adp').val();
+     if(arr != "")
+     if($(this).attr('id') == 'financial_year' && $('#financial_year').val() == projects[arr].financial_year){
+     $('#titleproject').val(projects[arr].name_of_scheme);
+     $('#summary_title').empty();
+     $('#label_summary_title').show('slow');
+     $('#summary_title').append("<label class=\"control-label\">"+$('#titleproject').val()+"</label>");
+
+     $('#original_cost').val(parseFloat(projects[arr].total_cost).toFixed(3));
+     $('#summary_original_cost').empty();
+     $('#label_summary_original_cost').show();
+     $('#summary_original_cost').append("<label class=\"control-label\">"+$('#original_cost').val()+"</label>");
+
+     $('#summary_districts').empty();
+     $("#districts").val($("#districts option").filter(function () { return $(this).html() == projects[arr].district; }).val());
+     $("#districts").select2();
+     values = $('#districts').find(':selected').text();
+     $("#label_summary_districts").show('slow');
+     $("#summary_districts").append("<label class=\"control-label\">"+values+"</label>");
+   }
+   }
+});
 $(document).ready(function(){
     $('button#add-more').click(function(e){
         var revised_Cost ='<div class="col-md-12"><div class="row"><div class="col-md-8">'
@@ -738,5 +789,6 @@ $(document).on('change','#gs_no,#financial_year',function(){
 //      $("#districts").val('').trigger('change');
 //    }
 // });
+
 </script>
 @endsection
