954a915ac6cba247ffb935ae42e046b4cccc1ca5
aihtshamali <aihtshamali@gmail.com>
2018-12-07 16:38:50 +0500

    vist

--- database/migrations/2018_11_07_154347_create_plantrip_purposes_table.php
+++ database/migrations/2018_11_07_154347_create_plantrip_purposes_table.php
@@ -15,18 +15,18 @@ class CreatePlantripPurposesTable extends Migration
     {
         Schema::create('plantrip_purposes', function (Blueprint $table) {
             $table->increments('id');
-            
+
             $table->integer('plantrip_triprequest_id')->unsigned()->index()->nullable();
-            $table->foreign('plantrip_triprequest_id')->references('id')->on('plantrip_triprequests')->onDelete('cascade');
+            $table->foreign('plantrip_triprequest_id')->references('id')->on('plantrip_triprequests')->onDelete('no action');
 
             $table->integer('plantrip_subcitytype_id')->unsigned()->index()->nullable();
-            $table->foreign('plantrip_subcitytype_id')->references('id')->on('plantrip_subcitytypes')->onDelete('cascade');
+            $table->foreign('plantrip_subcitytype_id')->references('id')->on('plantrip_subcitytypes')->onDelete('no action');
 
             $table->integer('plantrip_visitreason_id')->unsigned()->index()->nullable();
-            $table->foreign('plantrip_visitreason_id')->references('id')->on('plantrip_visitreasons')->onDelete('cascade');
+            $table->foreign('plantrip_visitreason_id')->references('id')->on('plantrip_visitreasons')->onDelete('no action');
 
             $table->integer('plantrip_purposetype_id')->unsigned()->index()->nullable();
-            $table->foreign('plantrip_purposetype_id')->references('id')->on('plantrip_purposetypes')->onDelete('cascade');
+            $table->foreign('plantrip_purposetype_id')->references('id')->on('plantrip_purposetypes')->onDelete('no action');
 
 
             $table->timestamps();
