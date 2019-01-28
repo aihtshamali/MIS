e4194e87d5bf2b184b079181ee1662b24af0ee0a
aihtshamali <aihtshamali@gmail.com>
2019-01-28 16:49:11 +0500

    helloBrother

--- app/VmisAssignedDriverLog.php
+++ app/VmisAssignedDriverLog.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class VmisAssignedDriver extends Model
+class VmisAssignedDriverLog extends Model
 {
     public function VmisRequestToTransportOfficerLog()
     {   
