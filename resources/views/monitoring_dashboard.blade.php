2fe2000f24898e3813550a33ba8057a6e1b2942f
aihtshamali <aihtshamali@gmail.com>
2019-02-06 15:59:16 +0500

    ok

--- resources/views/monitoring_dashboard.blade.php
+++ resources/views/monitoring_dashboard.blade.php
@@ -41,7 +41,7 @@ td{white-space: unset !important;}
                     <i class="feather icon-home bg-c-yellow card1-icon hidden-sm hidden-xs"></i>
                     <span class="text-c-yellow f-w-600 pointer"> <span style="color:black">Welcome</span>
                         @auth
-                          {{Auth::user()->first_name}} {{Auth::user()->last_name}}
+                          {{Auth::user()->first_name}} {{Auth::user()->last_name}} <small>({{Auth::user()->designation}})</small> 
                         @endauth <span style="color:black;">to Monitoring dashboard.</span>
                     </span>
                     <div class="progress clearfix mt2 clrornge">
