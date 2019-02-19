f1057d1cc322fd74c787eb8df88219cb05f40d33
Anas Majeed <anasmajeed.ch@gmail.com>
2019-02-19 14:11:16 +0500

    CHARTS MONITORING DASH

--- app/Http/Controllers/MonitoringChartController.php
+++ app/Http/Controllers/MonitoringChartController.php
@@ -25,14 +25,53 @@ class MonitoringChartController extends Controller
         ->where('projects.status',1)
         ->where('complete',1)->count();
 
+        //Chart TWO
+        $model = new User();
+        $officers = $model->hydrate(
+            DB::select(
+            'getAllOfficers'
+            )
+        );
+        $assigned_projects = [];
+        $actual_assigned_projects = [];
+        $team_lead=[];
+        $individual_projects=[];
+        foreach($officers as $officer){
+            if($officer->first_name == "Muhammad" || $officer->first_name == "Mohammad" || (preg_match('#M[u|o]hammad*#i', $officer->first_name)==1))
+            {
+                $officer->first_name = "M.";
+                // dd($officer);
+            }
+
+            $data = DB::select(
+                'MgetOfficersAssignedProjectById' .' '.$officer->id
+            );
+
+            $team_member = DB::select(
+                'MgetOfficersInProgressAsTeamMemberProjectsById' .' '.$officer->id
+            );
+            $team_l = DB::select(
+                'MgetOfficersInProgressAsTeamLeadProjectsById' .' '.$officer->id
+            );
+            $individual = DB::select(
+                'MgetOfficersInProgressAsIndividualProjectsById' .' '.$officer->id
+            );
+            array_push($actual_assigned_projects,$data);
+            // dd($data);
+            array_push($assigned_projects,count($team_member));
+            array_push($team_lead,count($team_l));
+            array_push($individual_projects,count($individual));
+        }
+
         \JavaScript::put([
-            // 'actual_total_projects' => $actual_total_projects,
             'total_projects' => $total_projects,
-            // 'total_assigned_projects' => $total_assigned_projects,
-            // 'actual_total_assigned_projects' => $actual_total_assigned_projects,
             'inprogress_projects' => $inprogress_projects,
             'completed_projects' => $completed_projects,
-            // 'officers' => $officers,
+            // chart two
+            'officers' => $officers,
+            'team_lead' => $team_lead,
+            'assigned_projects' => $assigned_projects,
+            'individual_projects' => $individual_projects
 
             ]);
         return view('_Monitoring/analytics/analytics');
@@ -55,14 +94,9 @@ class MonitoringChartController extends Controller
         ->where('complete',1)->count();
 
         \JavaScript::put([
-            // 'actual_total_projects' => $actual_total_projects,
             'total_projects' => $total_projects,
-            // 'total_assigned_projects' => $total_assigned_projects,
-            // 'actual_total_assigned_projects' => $actual_total_assigned_projects,
             'inprogress_projects' => $inprogress_projects,
             'completed_projects' => $completed_projects,
-            // 'officers' => $officers,
-
             ]);
         return view('_Monitoring/analytics/chart_one',compact('total_projects','inprogress_projects','completed_projects'));
     }
--- resources/views/_Monitoring/analytics/analytics.blade.php
+++ resources/views/_Monitoring/analytics/analytics.blade.php
@@ -25,7 +25,9 @@
   </div>
   <div class="col-md-4 pdlfrt1 mtb mxh3h mxh3h">
     <div class="border col-md-12">
-    <a href="{{route('m_chart_two')}}">Click</a>
+    <a href="{{route('m_chart_two')}}">
+          <div id="chartdiv2"></div>
+    </a>
     </div>
   </div>
   <div class="col-md-4 pdlfrt1 mtb mxh3h mxh3h">
@@ -101,5 +103,131 @@ series.columns.template.fillOpacity = .8;
 var columnTemplate = series.columns.template;
 columnTemplate.strokeWidth = 2;
 columnTemplate.strokeOpacity = 1;
+</script>
+
+{{-- //CHART TWO --}}
+<script>
+  function loadDataTable(){
+    // $('table.officer_projects').DataTable({
+    //   dom: 'Bfrtip',
+    //       buttons: [
+    //           'copy', 'csv', 'excel', 'pdf', 'print'
+    //       ]
+    // });
+    // $('.dt-buttons').addClass('col-md-6')
+    // $('.dataTables_filter').addClass('col-md-6').css('text-align','right');
+  }
+
+let chart2 = am4core.create("chartdiv2", am4charts.XYChart);
+
+var st = [];
+        $i = 0;
+        officers.forEach(element => {
+          st.push ({
+            "Name":element.first_name +" "+element.last_name,
+            "userid":element.id,
+            "Team Member": assigned_projects[$i],
+            "Team Lead" : team_lead[$i],
+            "Team Member": assigned_projects[$i],
+            "Individual Projects" : individual_projects[$i]
+          });
+          $i++;
+        });
+
+// Add data
+chart2.data = st;
+
+// Create axes
+let categoryAxis2 = chart2.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis2.dataFields.category = "Name";
+categoryAxis2.renderer.grid.template.location = 0;
+categoryAxis2.renderer.minGridDistance = 30;
+// Label
+var label = categoryAxis.renderer.labels.template;
+label.wrap = false;
+label.maxWidth = 50;
+label.verticalCenter = "middle";
+
+// Rotation
+categoryAxis2.renderer.labels.template.rotation=45;
+
+let valueAxis2 = chart2.yAxes.push(new am4charts.ValueAxis());
+valueAxis2.renderer.inside = true;
+valueAxis2.renderer.labels.template.disabled = true;
+valueAxis2.min = 0;
+
+
+// Modal
+// var temp=new Vue({
+//        el: '.modal-body',
+//        data: {
+//          officer_projects:[],
+//          officer_name:''
+//        },
+//        methods: {
+//         fillModal(data,name){
+           
+//            this.officer_projects=data
+//            this.officer_name=name
+//            loadDataTable()      
+//          }
+//        },
+//      })
+
+// Create series
+function createSeries(field, name,color="#67b7dc") {
+
+  // Set up series
+  let series = chart2.series.push(new am4charts.ColumnSeries());
+  series.name = name;
+  series.dataFields.valueY = field;
+  series.dataFields.categoryX = "Name";
+  series.sequencedInterpolation = true;
+
+  // Make it stacked
+  series.stacked = true;
+
+  // Configure columns
+  series.columns.template.width = am4core.percent(60);
+  series.columns.template.tooltipText = "[bold]{name}[/]\n[font-size:14px]{categoryX}: {valueY}";
+
+  // Color
+  series.columns.template.fill = am4core.color(color); // fill
+
+  // Add label
+  let labelBullet = series.bullets.push(new am4charts.LabelBullet());
+  labelBullet.label.text = "{valueY}";
+  labelBullet.locationY = 0.5;
+
+  
+
+  // OnCLick EVENT
+  series.columns.template.events.on("hit", function(ev) {
+    var name=ev.target.dataItem._dataContext.Name;
+    axios.get('{{route("getOfficerProjects")}}',{
+      params: {
+        'user':ev.target.dataItem._dataContext.userid,
+        '_token':"{{csrf_token()}}",
+        'status': series.name.replace(" ","").replace("Projects","")
+      }
+    })
+    .then((response) => {
+      var projects=response.data;
+      temp.fillModal(projects,name)
+      $("#OfficerProjectModal").modal('show');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }, this);
+  return series;
+}
+
+var series1=createSeries("Team Lead", "Team Lead","#0062b7");
+var series2=createSeries("Team Member", "Team Member","#929eaa");
+createSeries("Individual Projects", "Individual Projects","#00a65a");
+
+// Legend
+// chart2.legend = new am4charts.Legend();
 </script>
 @endsection
