5e6229796c5bce154a4ca536ae693a30180a79f4
AymunSaif <aymun.saif18@yahoo.com>
2019-07-10 09:50:16 +0500

    newcode

--- routes/web.php
+++ routes/web.php
@@ -218,6 +218,9 @@ Route::prefix('Monitorofficer')->middleware('role:monitor|officer')->group(funct
   Route::get('/monitoring_inprogressAssignment','OfficerController@monitoring_inprogressAssignments')->name('Monitoring_inprogressAssignments');
   Route::post('/monitoring_inprogress_costs_saved','OfficerController@monitoring_inprogress_costs_saved')->name('Monitoring_inprogressCostSaved');
   Route::post('/monitoring_inprogress_dates_saved','OfficerController@monitoring_inprogress_dates_saved')->name('Monitoring_inprogressDateSaved');
+  
+  Route::post('/Monitoring_PlannedDates','OfficerController@Monitoring_PlannedDates')->name('Monitoring_PlannedDates');
+  
   Route::post('/monitoring_inprogress_organizations_saved','OfficerController@monitoring_inrogress_organizations_saved')->name('Monitoring_inprogressOrganizationSaved');
   Route::post('/monitoring_inprogress_location_saved','OfficerController@monitoring_inprogress_location_saved')->name('Monitoring_inprogressLocationSaved');
   Route::get('/monitoring_completedAssignment','OfficerController@monitoring_completedAssignments')->name('Monitoring_completedAssignments');
