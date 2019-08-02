0bd48fb4d3d70b08210d8528a40eb3094dfa2409
AymunSaif <aymun.saif18@yahoo.com>
2019-08-02 12:46:02 +0500

    newchanges

--- resources/views/executive/home/chart_eight.blade.php
+++ resources/views/executive/home/chart_eight.blade.php
@@ -123,7 +123,7 @@
               "Assigned Projects": assignedprojects_wrt_sectors[$i][0].eachproject,
               "Inprogress Projects": inprogressprojects_wrt_sectors[$i][0].eachinprogressproject,
               "Completed Projects": completedprojects_wrt_sectors[$i][0].eachcompletedproject,
-              "Stopped Project": stoppedProjects_wrt_sectors[$i][0].eachStoppedproject
+             
             });
             $i++;
           });
