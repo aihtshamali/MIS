8aa33cc581eb1cb72d2c89ba4d7cd47049b861bb
DGME _IT <dgme_it@DGME.GOV.PK>
2019-08-09 12:34:37 +0500

    changes by aymun

--- resources/views/_Monitoring/inc/monitoring/planmonitoring.blade.php
+++ resources/views/_Monitoring/inc/monitoring/planmonitoring.blade.php
@@ -522,7 +522,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <button type="submit" id="customButton"  onclick="return confirm('Are You Sure?')"  class="btn btn-success pull-right mr-5">SAVE</button>
+                                <button type="submit" id="customButton"   class="btn btn-success pull-right mr-5">SAVE</button>
                                 </form>
                         </div>
                     </div>
