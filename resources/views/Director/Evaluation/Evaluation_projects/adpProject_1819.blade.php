053b558cae0de27586290dfce569bc6c412cd2e3
dgme_it1 <dgme_it1@DGME.GOV.PK>
2020-01-08 11:20:25 +0500

    newchanges

--- resources/views/Director/Evaluation/Evaluation_projects/adpProject_1819.blade.php
+++ resources/views/Director/Evaluation/Evaluation_projects/adpProject_1819.blade.php
@@ -10,8 +10,13 @@
 
       }
        .table.dataTable td, .table.dataTable th {
-         text-align:LEFT !important;
-    font-size: 14px !important;
+         text-align:center !important;
+    font-size: 13px !important;
+    vertical-align: middle !important;
+    }
+    hr{
+      margin-top: 5px !important;
+margin-bottom: 5px !important;
     }
   </style>
 @endsection
@@ -19,10 +24,6 @@
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-
-
-
   <section class="content">
       {{--  sekect consulatants  --}}
       <div class="row">
@@ -30,7 +31,7 @@
           {{--  Chart 1  --}}
           <div class="box box-warning">
             <div class="box-header with-border">
-              <h3 class="box-title"><b>COMPLETED PROJECTS</b></h3>
+              <h3 class="box-title"><b>2018-2019 PROJECTS</b></h3>
               <button class="btn btn-danger" style="color:white;font-weight:bold font-size:20px;">@if(isset($projects)){{$projects->count()}}@endif</button>
 
               <div class="box-tools pull-right">
@@ -42,26 +43,26 @@
 
             <div class="box-body">
                 <div class="table-responsive">
-                  <table id="example1" data-page-length="50" class="table table-hover table-bordered table-striped">
+                  <table id="example1" data-page-length="100" class="table compact table-bordered ">
                     <thead>
                         <tr >
                             <th>Project #</th>
-                            <th style="width:25% !important;">Project Name</th>
+                            <th>ADP</th>
+                            <th style="width:15% !important; " >Project Name</th>
                             <th>Project Officers</th>
                             <th>SNE</th>
-                            <th style="width:12% !important;">Subsectors</th>
-                            <th>Date Start</th>
-                            <th>Date End</th>
-                            <th>Score</th>
+                            <th >Subsectors</th>
+                            <th>Planned Gestation Period</th>
                             <th>Progress</th>
                           </tr>
                     </thead>
 
                   <tbody>
                     @foreach ($projects as $project)
-                      <tr @if($project->progress < 100) style="background:red;color:white" @endif>
+                      <tr>
                         <td>{{ $project->project->project_no }}</td>
-                      <td>{{ $project->project->title }}</td>
+                          <td>{{ $project->project->ADP }}/{{ $project->project->financial_year }}</td>
+                      <td style="text-align:left !important;">{{ $project->project->title }}</td>
                       <td>
                      <ul>
                         @foreach ($project->AssignedProjectTeam as $team)
@@ -79,21 +80,28 @@
                     </td>
                     <td>{{$project->project->ProjectDetail->sne}}</td>
                     <td>
-                        <ul>
-                        @foreach ($project->project->AssignedSubSectors as $item)
-                              <li>{{$item->SubSector->name}}</li>
-                        @endforeach
-                        </ul>
+                      <span class="">
+                           @foreach ($project->project->AssignedSubSectors as $item)
+                         <b>{{$item->SubSector->sector->name}}</b>
+                         @endforeach
+                      </span>
+                     <hr>
+                     <ul>
+                       @foreach ($project->project->AssignedSubSectors as $item)
+                           <li>{{$item->SubSector->name}}</li>
+                       @endforeach
+                     </ul>
                     </td>
-                    <td>
-                    {{date('d-M-y',strtotime($project->created_at))}}
+                    <td class="text-center">
+                      Start Date : <b>{{date('d-M-y',strtotime($project->project->ProjectDetail->planned_start_date))}}</b> <br>
+                      End Date : <b>{{date('d-M-y',strtotime($project->project->ProjectDetail->planned_end_date))}}</b> <br>
+                      @php
+                        $interval = date_diff( date_create(date('d-M-y h:i:s',strtotime($project->project->ProjectDetail->planned_start_date))),
+                        date_create(date('d-M-y h:i:s',strtotime($project->project->ProjectDetail->planned_end_date))))->format('%y Year %m Month %d Day %h Hours');
+                      @endphp
+                      {{$interval}}
                     </td>
-                    <td>
-                    {{ date('d-M-y',strtotime($project->updated_at)) }}
-                    </td>
-                    <td>
-                      {{ round($project->project->score,2,PHP_ROUND_HALF_UP) }}
-                    </td>
+
                     <td>
                       {{ round($project->progress,2,PHP_ROUND_HALF_UP) }}
                     </td>
